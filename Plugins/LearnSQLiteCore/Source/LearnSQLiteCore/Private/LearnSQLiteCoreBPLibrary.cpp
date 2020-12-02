e989cf8fdded8c9206d16902e01982b223b99ee6
Michael Peacock <sc4ler@gmail.com>
2020-12-01 20:58:58 -0600

    Updated LearnSQLiteCore with execute and query tests

--- Plugins/LearnSQLiteCore/Source/LearnSQLiteCore/Private/LearnSQLiteCoreBPLibrary.cpp
+++ Plugins/LearnSQLiteCore/Source/LearnSQLiteCore/Private/LearnSQLiteCoreBPLibrary.cpp
@@ -2,7 +2,7 @@
 #include "LearnSQLiteCoreBPLibrary.h"
 #include "LearnSQLiteCore.h"
 #include "SQLiteCore/Public/SQLiteTypes.h"
-//    EQLiteColumnType
+//    ESQLiteColumnType
 #include "SQLiteCore/Public/SQLiteDatabase.h"
 //    ESQLiteDataBaseOpenMode
 //    FSQLiteDatabase
@@ -17,19 +17,114 @@ ULearnSQLiteCoreBPLibrary::ULearnSQLiteCoreBPLibrary(const FObjectInitializer& O
 void ULearnSQLiteCoreBPLibrary::Alpha()
 {
     // Create Database
+    UE_LOG(LogLearnSQLiteCore, Warning, TEXT("Creating sqlite database object"));
     FSQLiteDatabase db = FSQLiteDatabase();
 
     // Open Database
-    const TCHAR* DatabaseFilename = TEXT("C:\\example.db");
+    // @note LogSQLiteDatabase: Warning: Failed to open database 'D:\db\example.db': disk I/O error
+    UE_LOG(LogLearnSQLiteCore, Warning, TEXT("Opening database: example.db"));
+    const TCHAR* DatabaseFilename = TEXT("D:\\db\\example.db");
     const ESQLiteDatabaseOpenMode OpenMode = ESQLiteDatabaseOpenMode::ReadWriteCreate;
-    bool IsOpen = db.Open(DatabaseFilename, OpenMode);
+    const bool IsOpen = db.Open(DatabaseFilename, OpenMode);
     UE_LOG(LogLearnSQLiteCore, Warning, TEXT("Alpha(): db.Open() = %d"), IsOpen);
     if( !IsOpen )
     {
+        UE_LOG(LogLearnSQLiteCore, Warning, TEXT("Failed to open example.db"));
         return;
     }
 
+    // Perform Operations
+    if( db.IsValid() )
+    {
+        // Execute (1/n)
+        // @note LogSQLitePreparedStatement: Warning: Failed to create prepared statement from 'CREATE TABLE Alpha(a0 INT PRIMARY KEY NOT NULL, a1 TEXT)': table Alpha already exists
+        UE_LOG(LogLearnSQLiteCore, Warning, TEXT("Creating table Alpha"));
+        const TCHAR* SqlCreateTableAlpha = TEXT("CREATE TABLE Alpha(a0 INT PRIMARY KEY NOT NULL, a1 TEXT)");
+        const int64 RowsEnumerated = db.Execute(SqlCreateTableAlpha);
+        
+        // Execute (2/n)
+        // @note: Data was populated with external database editor
+        // @note: If the query result was empty, the callback was not executed, i.e. an empty table.
+        UE_LOG(LogLearnSQLiteCore, Warning, TEXT("Executing 'SELECT * FROM Alpha'..."));
+        const TCHAR* SqlSelectAllFromAlpha = TEXT("SELECT * FROM Alpha");
+        const int64 RowsEnumerated2 = db.Execute( SqlSelectAllFromAlpha, [](const FSQLitePreparedStatement& InStatement)
+        {
+            // @return ESQLitePreparedStatementExecuteRowResult (Continue, Stop, Error)
+            // @note Stop does not return an error
+            // @note Error also stops
+                
+            const TArray<FString>& ColumnNames = InStatement.GetColumnNames();
+            UE_LOG(LogTemp, Warning, TEXT("ColumnNames:"));
+            for(const FString& ColumnName : ColumnNames )
+            {
+                UE_LOG(LogTemp, Warning, TEXT("\t%s"), *ColumnName);
+            }
+
+            //@todo use this
+            // const TCHAR* BindingName = TEXT("");
+            // const int32 GetBindingIndexByNameResult = InStatement.GetBindingIndexByName(BindingName);
+
+            //@todo use this
+            // const TCHAR* InColumnName = TEXT("");
+            // const int32 GetColumnIndexByNameResult = InStatement.GetColumnIndexByName(InColumnName);
+
+            //@todo use this
+            // ESQLiteColumnType OutColumnType;
+            // const bool GetColumnTypeByIndexResult = InStatement.GetColumnTypeByIndex(/** column index */ 0, OutColumnType);
+            // const bool GetColumnTypeByNameResult = InStatement.GetColumnTypeByName(InColumnName, OutColumnType);
+
+            // Out Values 
+            //      int8, uint8,
+            //      int16, uint16,
+            //      int32, uint32,
+            //      int64, uint64,
+            //      FDateTime,
+            //      EnumType
+            //      float, double,
+            //      FString, FName, FText,
+            //      TArray<uint8>
+            //      FGuid
+            // Reference: SQLitePreparedStatement.h line 228 to 281
+            int8 OutValueInt8;
+            bool GetColumnValueByIndexResult = InStatement.GetColumnValueByIndex(/** column index */ 0, OutValueInt8);
+            // ...
+            
+            return ESQLitePreparedStatementExecuteRowResult::Continue;
+        });
+
+        const FString LastError = db.GetLastError();
+        UE_LOG(LogLearnSQLiteCore, Warning, TEXT("Last Error: %s"), *LastError);
+        
+        const int64 LastInsertRowId = db.GetLastInsertRowId();
+        UE_LOG(LogLearnSQLiteCore, Warning, TEXT("Last Insert Row Id: %d"), LastInsertRowId);
+    }
+    
     // Close Database
-    bool IsClosed = db.Close();
-    UE_LOG(LogLearnSQLiteCore, Warning, TEXT("Alpha(): db.Close() = %d"), IsClosed);
+    if( db.IsValid() )
+    {
+        const bool IsClosed = db.Close();
+        UE_LOG(LogLearnSQLiteCore, Warning, TEXT("Alpha(): db.Close() = %d"), IsClosed);
+    }
 }
+
+/**
+ ** References **
+ *
+ * - Epic Games, Inc. "Unreal Engine 4.6 Release Notes"
+ *		https://docs.unrealengine.com/en-US/Support/Builds/ReleaseNotes/2014/4_6/index.html
+ *		Search for 'TFunction' and 'TFunctionRef'
+ *
+ * - Epic Games, Inc. "TFunctionRef"
+ *      https://docs.unrealengine.com/en-US/API/Runtime/Core/GenericPlatform/TFunctionRef/index.html
+ *
+ * - Epic Games, Inc.
+ *      "Engine\Source\Runtime\Core\Public\Templates\Function.h". line 793-843
+ *      Contains documentation on usage of TFunction and TFunctionRef in comments above class definitions
+ *
+ * - answers.unrealengine.com. "How to pass a function as parameter of UFUNCTION() function?"
+ *      https://answers.unrealengine.com/questions/758353/view.html
+ *
+ * - forums.unrealengine.com. "TFunctionRef becomes nullptr after timeline finished inside a lambda. Is this behaviour intended?"
+ *      https://forums.unrealengine.com/development-discussion/c-gameplay-programming/1691175-tfunctionref-becomes-nullptr-after-timeline-finished-inside-a-lambda-is-this-behaviour-intended
+ *
+ */
